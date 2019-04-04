a36f111df47d2ff6b65c73f955adab60f2f3dfae
George St <47747179+gstathoulopoulos@users.noreply.github.com>
2019-04-04 16:31:25 +0300

    2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+//import { render } from "react-dom";
 
 import Titles from "./components/Titles";
 import Form from "./components/Form";
@@ -11,17 +12,20 @@ const USERNAME = "bigbearanesq1z";
 class App extends React.Component {
   getInfo = async e => {
     e.preventDefault();
+    console.log(`***Before`);
+    console.log(`** componentWillMount`);
     const Title = e.target.elements.Title.value;
     const api_call = await fetch(
       `https://api.thetvdb.com/search/series?name=${Title}`
     );
     //const data = await api_call.json();
     const response = await api_call.response();
-    console.log("jdklsasjdla");
+    console.log(`***After`);
     //console.log(response);
     //console.log(data);
   };
   render() {
+    console.log(`**** render`);
     return (
       <div>
         <Titles />
