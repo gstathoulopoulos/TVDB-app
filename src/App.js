a9dfc08c5ceaf516049dcd0104901b54ddfedfab
George St <47747179+gstathoulopoulos@users.noreply.github.com>
2019-04-04 12:46:55 +0300

    .

--- src/App.js
+++ src/App.js
@@ -4,18 +4,22 @@ import Titles from "./components/Titles";
 import Form from "./components/Form";
 //import Weather from "./components/Weather";
 
-const API_KEY = "";
+const API_KEY = "IY3JFFL7JRCNNA7V";
+const USER_KEY = "0VLSLJEEW1KO94E8";
+const USERNAME = "bigbearanesq1z";
 
 class App extends React.Component {
   getInfo = async e => {
     e.preventDefault();
-    const city = e.target.elements.city.value;
-    const country = e.target.elements.country.value;
+    const Title = e.target.elements.Title.value;
     const api_call = await fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`
+      `https://api.thetvdb.com/search/series?name=${Title}`
     );
-    const data = await api_call.json();
-    console.log(data);
+    //const data = await api_call.json();
+    const response = await api_call.response();
+    console.log("jdklsasjdla");
+    //console.log(response);
+    //console.log(data);
   };
   render() {
     return (
